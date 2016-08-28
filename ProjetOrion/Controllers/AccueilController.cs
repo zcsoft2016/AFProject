74e34deb3fd3956b2b473503dedd2ebe681e3adc
bobisolo <zongojoe@yahoo.fr>
2016-08-28 01:40:46 +0100

    Premier commit. Creation du projet et mise en place du menu "commercial"

---
+++ ProjetOrion/Controllers/AccueilController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetOrion.Controllers
+{
+    public class AccueilController : Controller
+    {
+        // GET: Accueil
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ProjetOrion/Controllers/CommercialController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetOrion.Controllers
+{
+    public class CommercialController : Controller
+    {
+        // GET: Commercial
+        public ActionResult IndexCommercial()
+        {
+            return View();
+        }
+
+    }
+}
